fccf280d5eb29c19b1c1736dbb9ada5a0e6b108d
Victor <victorvelas.lst@gmail.com>
2026-06-15 22:35:59 -0500

    feat: add ContactForm component with base structure and styling

--- app/components/Contact/ContactForm.vue
+++ app/components/Contact/ContactForm.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-    <section class="p-6 rounded-2xl border light:border-gray-200/90  dark:border-main-500 backdrop-blur-sm bg-white/3 min-h-[50vh] shadow-lg shadow-main-500/20">
-        <form class="flex flex-col gap-3 ">
+    <section>
+        <form class="w-14/15 md:w-5/6 m-auto flex flex-col gap-3 p-6 rounded-2xl border light:border-gray-200/90  dark:border-main-500 backdrop-blur-sm bg-white/3 min-h-[50vh] shadow-lg shadow-main-500/20">
             
         </form>
     </section>
-</template>
+</template> 
